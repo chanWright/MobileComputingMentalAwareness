66e223c533247c258a921f55660e79a0e6460ae7
Chandler Wright <chanwright@sbcglobal.net>
2020-10-29 05:17:49 -0500

    Started working on game first game idea
    The current game idea is that image objects will move around the screen for 60 seconds, when the user taps on the image it will make it fade away and adds a point to the score. I may change this to make them motionless and have no time limit. This is designed as a distraction game and may not need a time limit and can go on for as long as needed

--- AndroidProject/app/src/main/java/com/example/mentalawareness/GameActivity.java
+++ AndroidProject/app/src/main/java/com/example/mentalawareness/GameActivity.java
@@ -3,11 +3,24 @@ package com.example.mentalawareness;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.Timer;
+
+public class GameActivity extends AppCompatActivity implements Runnable{
     DrawerLayout dlayout;
+
+    ImageView[] gameObjects = new ImageView[4];
+    TextView scoreTV;
+    TextView timerTV;
+    double timer = 0.0;
+    double maxTime = 60.0;
+    double startTime = 0.0;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +28,17 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         dlayout = findViewById(R.id.drawer);
-
+        gameObjects[0] = findViewById(R.id.gameObject);
+        gameObjects[1] = findViewById(R.id.gameObject1);
+        gameObjects[2] = findViewById(R.id.gameObject2);
+        gameObjects[3] = findViewById(R.id.gameObject3);
+        scoreTV = findViewById(R.id.scoreTextView);
+        timerTV = findViewById(R.id.timerTextView);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(gameObjects[0],"translationX",100f);
+        animator.setDuration(2000);
+        animator.start();
+        startTime = System.nanoTime() / 1000000;
+        run();
     }
 
     // Code for Navigation Begins Here
@@ -56,6 +79,22 @@ public class GameActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         MainActivity.closeDrawer(dlayout);
+    }
+
+    @Override
+    public void run() {
+        while (timer <= maxTime){
+            timer = ((System.nanoTime() / 1000000) - startTime);
+            timerTV.setText("Timer: "+(maxTime-timer));
+        }
+        if (timer >= maxTime){
+            for (ImageView object:gameObjects)
+            {
+                while(object.getAlpha() > 0) {
+                    object.setAlpha(object.getAlpha() - 0.1f);
+                }
+            }
+        }
     }
     // Code for Navigation Ends Here
 
