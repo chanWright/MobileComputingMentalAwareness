338ff970b885dfe87affb6e09a16663c85a922e9
Oluwayemisi Runsewe <49408972+Programmer026@users.noreply.github.com>
2020-11-20 17:41:04 -0600

    Links open on webPages

--- AndroidProject/app/src/main/java/com/example/mentalawareness/SourcesActivity.java
+++ AndroidProject/app/src/main/java/com/example/mentalawareness/SourcesActivity.java
@@ -1,11 +1,17 @@
 package com.example.mentalawareness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class SourcesActivity extends AppCompatActivity {
@@ -14,6 +20,32 @@ public class SourcesActivity extends AppCompatActivity {
     private RecyclerView sourseRV = null;
     private SourcesModel myModel = null;
     private SourceAdapter myAdapter;
+    private GestureDetectorCompat detector = null;
+
+    private class RecyclerViewOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            View view = sourseRV.findChildViewUnder(e.getX(), e.getY());
+            if (view != null) {
+                RecyclerView.ViewHolder holder = sourseRV.getChildViewHolder(view);
+                if (holder instanceof SourceAdapter.SourceViewHolder) {
+                    int position = holder.getAdapterPosition();
+                    // handle single tap
+                    Log.d("click", "clicked on item "+ position);
+                    Log.d("click", "name of item was" + myModel.myCite.get(position).link);
+
+                    Intent implicict = new Intent(Intent.ACTION_VIEW);
+                    Uri uri = Uri.parse(myModel.myCite.get(position).link);
+                    implicict.setData(uri);
+                    startActivity(implicict);
+
+                    return true; // Use up the tap gesture
+                }
+            }
+            // we didn't handle the gesture so pass it on
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +61,16 @@ public class SourcesActivity extends AppCompatActivity {
         sourseRV.setAdapter(myAdapter);
         LinearLayoutManager lin = new LinearLayoutManager(this);
         sourseRV.setLayoutManager(lin);
+
+        detector = new GestureDetectorCompat(this,
+                new RecyclerViewOnGestureListener());
+        // add the listener to the recycler
+        sourseRV.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener(){
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                return detector.onTouchEvent(e);
+            }
+        });
     }
 
     // Code for Navigation Begins Here
